888811e783c89ad30e3f59000526fdff814871ed
Sarah Massengill <sarahmassengill@gmail.com>
2020-11-24 16:42:07 -0800

    more work on the readme, writing codebook, adding cleaned data files.

--- gettingCleaningdata/ReadMe.md
+++ gettingCleaningdata/ReadMe.md
@@ -1,4 +1,4 @@
-
+# ReadMe
 ## Purpose:
 This is a project for the 3rd course in Johns Hopkins University's Data Science 
 Specialization offered by [Coursera](http://coursera.org).  The projects requires
@@ -6,14 +6,18 @@ that data to be gathered and tidied with the following stipulations:
 
 Create one R script called run_analysis.R that does the following.
 
-        1. Merge the training and the test sets to create one data set.
-        2. Extract only the measurements on the mean and standard deviation 
-           for each measurement.
-        3. Use descriptive activity names to name the activities in the data set
-        4. Appropriately labels the data set with descriptive variable names.
-        5. From the data set in step 4, creates a second, independent tidy data 
-           set with the average of each variable for each activity and each 
-           subject.
+0. Read in all the necessary labels, data, column names, and subject Id's
+   Appropriately labels the data set with descriptive variable names.(part of #4)
+1. Merge the training and the test sets to create one data set.
+2. Extract only the measurements on the mean and standard deviation for 
+   each measurement.
+3. Use descriptive activity names to name the activities in the data set 
+4. Format the descriptive variable names that were added in #0 so that they can 
+   be conveniently used with out being encased in ``. To do this the dashes, -, 
+   and the parentheses,(), need to be removed from the variable names.
+5. From the data set in step 4, create a second, independent tidy data 
+   set with the average of each variable for each activity and each 
+   subject.
 
 
 ## Raw data:
@@ -23,8 +27,16 @@ Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ort
 
 The raw data is stored in the [UCI HAR Dataset directory](https://github.com/sarahmass/dataScienceCoursera/tree/main/gettingCleaningdata/UCI%20HAR%20Dataset)
 
-Within the UCI HAR Dataset directory, there is a [ReadMe](https://github.com/sarahmass/dataScienceCoursera/blob/main/gettingCleaningdata/UCI%20HAR%20Dataset/README.txt) describing the data and how it was collected.  The data that has been drawn upon for this project is found in both the train and test folders.  Since we are gathering data to anazyse it rather than to train a Classification Neural Neural Network the data needs to be brought together into a single dataframe, participant id's, the activity, and collected feature values need to be pulled together first.  The participants' id's have been separated from the collected metrics because the ID is needed for the job of classifying each activity. ID's are saved in the files called subject_test.txt and subject_train.txt.  Since the Neural Network was being trained to take in the collected metrics to classify the activity as Walking, Walking Upstairs, Walking Downstairs, or laying down, the actual activity tags were separated out of the data file as well to be used in the error analysis of the Neural Network.  The activities for each experiment(or row of ID and Data) can be found in the files labeled y_test.txt and y_train.txt.  The collected metrics are found in the files X_test.txt and X_   
+Within the UCI HAR Dataset directory, there is a [ReadMe](https://github.com/sarahmass/dataScienceCoursera/blob/main/gettingCleaningdata/UCI%20HAR%20Dataset/README.txt) describing the data and how it was collected.  The subject ID's, normalized data, and activity labels will be found with in the sub-directories: [train](https://github.com/sarahmass/dataScienceCoursera/tree/main/gettingCleaningdata/UCI%20HAR%20Dataset/train) and [test](https://github.com/sarahmass/dataScienceCoursera/tree/main/gettingCleaningdata/UCI%20HAR%20Dataset/test).
 
+The original purpose of the data was to use it to train a Neural Network to classify the activity being performed by each subject.  It was separated into two sets, a training set and a testing set.  The Neural Network required that the Id's, collected metrics, and the labeled outcome be saved in separate places.  Within their respective sub-directory the ID's can be found in the files labeled subject_train.txt and subject_test.txt, the data with 561 columns of collected metrics can be found in X_train.txt and X_test.txt, and the activity labels are in y_train.txt and y_test.txt.  Before the testing and training data can be merged the ID, activity, and data columns need to be merged together, and then the rows for the testing and training subjects can be merged.  
+
+The metric data collected for each subject for each activity have the 561 row names found in the feature.txt file in the UCI HAR Dataset Directory.  The metric variables are described in the feature_info.txt file.  
+
+  
+## Getting and Cleaning the Data:
+
+The raw data as described above will be cleaned as described in the Purpose section.  I have included a [CodeBook.md]({add link}) to detail my process, the code in [run_analysis.R]({add link}), and the two data sets in {fill in the file names and links here}.
 
 
 
--- gettingCleaningdata/run_analysis.R
+++ gettingCleaningdata/run_analysis.R
@@ -13,9 +13,13 @@
 # Neural Networks, Computational Intelligence and Machine Learning, 
 # ESANN 2013. Bruges, Belgium 24-26 April 2013.
 
-# STEP 0: Collect the Id labels, activity labels, column labels, test data
-#         train data stored with in the directory "UCI HAR Dataset" with in the
-#         working directory,"dataScienceCoursera/gettingCleaningdata"
+library(dplyr)
+
+################################################################################
+# STEP 0: Collect the Id labels, activity labels, column labels, test data     #
+#         train data stored with in the directory "UCI HAR Dataset" with in the#
+#         working directory,"dataScienceCoursera/gettingCleaningdata"          #
+################################################################################
 
 # Gather the 561 column names for the test and train data sets
 colNames<- read.table("./UCI HAR DATAset/features.txt")
@@ -31,6 +35,8 @@ test_data <- read.table("./UCI HAR DATAset/test/X_test.txt")
 
 # combine the test data with the subject Id, and the activity label
 test_data <- cbind(test_subjectId[,1], test_activity[,1], test_data)
+
+# Appropriately labels the data set with descriptive variable names.
 names(test_data) <- c("subjectId", "activity", colNames[,2] )
 
 # Gather the subject ID's for the train data
@@ -44,12 +50,17 @@ train_data <- read.table("./UCI HAR DATAset/train/X_train.txt")
 
 # Combine the train data with the subject Id, and the activity label
 train_data <- cbind(train_subjectId[,1], train_activity[,1], train_data)
+
+# Appropriately labels the data set with descriptive variable names.
 names(train_data) <- c("subjectId", "activity", colNames[,2] )
 
 #clean up temporary variables
 rm(test_subjectId, test_activity, train_subjectId, train_activity, colNames)
 
-# STEP 1: Merges the training and the test sets to create one data set.
+###############################################################################
+# STEP 1: Merges the training and the test sets to create one data set.       #
+###############################################################################
+
 data <- rbind(train_data, test_data)
 
 # clean up temporary data variables
@@ -58,11 +69,54 @@ rm(test_data, train_data)
 
 
 
+################################################################################
+# STEP 2: Extracts only the measurements on the mean and standard deviation    #  
+#         for each measurement.                                                #
+################################################################################
 
-# STEP 2: Extracts only the measurements on the mean and standard deviation for each 
-#         measurement.
-library(dplyr)
-data <- select(data,grep("mean|std",names(data),value = T))
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names.
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+data <- select(data,subjectId, activity, grep("mean|std",names(data),value = T))
+
+################################################################################
+# STEP 3: Uses descriptive activity names to name the activities in the data   #
+#         set. The activity codes and labels are stored in a file. The file is #
+#         read in and the labels are used to convert the activities from       #
+#         numeric codes to factor labels.                                      #
+################################################################################
+
+# Read activity code and label pairs from file
+activity_tbl <- read.table("./UCI HAR DATAset/activity_labels.txt",
+                              col.names=c("code", "label"))
+
+# Activity Labels: WALKING, wALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING,
+# STANDING, and LAYING.
+data <- mutate(data, activity = cut(data$activity,breaks = 6,
+                                    labels = activity_tbl$label))
+
+################################################################################
+# STEP 4: Appropriately label variables with descriptive names. This was mostly#
+#         done in step 0. Here the variable names are fixed to exclude symbols #
+#         dashes and parentheses to make them more convenient for further      #
+#         analysis (they won't need to be encased in ``).                      #
+################################################################################
+
+names(data) <- gsub("\\()","", names(data))
+names(data) <- gsub("-", ".", names(data))
+
+# Write data set to a .csv file:
+write.csv(data, file = "subActivity_all.csv", row.names = FALSE)
+
+
+################################################################################
+# STEP 5: From the data set in step 4, creates a second, independent tidy data #  
+#         set with the average of each variable for each activity and each     #
+#         subject.                                                             #
+################################################################################
+
+# Group by subjectId and activity, then find the mean of each variable for each 
+# subjectId, activity pair. 
+
+SubActivityMeans <- data %>% group_by(subjectId, activity) %>%
+                             summarize_all(mean,.groups="keep")
+
+#write dataframe to .csv file:
+write.csv(SubActivityMeans, file="SubActivity_means.csv", row.names = FALSE)
